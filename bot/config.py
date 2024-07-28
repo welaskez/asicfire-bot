91a00dda22eaae705117c9982e73ad768d380f2d
welaskez <nazar.nikitenko.03@mail.ru>
2024-07-28 12:16:08 +0500

    add channel_username to env

--- bot/config.py
+++ bot/config.py
@@ -12,5 +12,7 @@ ADMINS = list(map(lambda admin_id: int(admin_id), os.getenv("ADMINS").split(" ")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+CHANEL_USERNAME = os.getenv("CHANEL_USERNAME")
+
 DB_URL = os.getenv("DB_URL")
 DB_ECHO = True
