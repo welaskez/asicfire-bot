ccb4ffe25f5dd24d17122cedd4e0b4979b41dc44
welaskez <nazar.nikitenko.03@mail.ru>
2024-07-30 06:22:44 +0500

    init db module

---
+++ bot/db/engine.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+import config
+
+engine = create_async_engine(
+    config.DB_URL,
+    echo=config.DB_ECHO,
+    pool_size=5,
+    max_overflow=10,
+)
+
+session_maker = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
