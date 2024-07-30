efb7495148236927405ccac0bff89c8f2dd363c3
welaskez <nazar.nikitenko.03@mail.ru>
2024-07-30 07:37:36 +0500

    add posts crud

---
+++ bot/db/crud/post_requests.py
@@ -0,0 +1,40 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models import Post
+
+
+async def create_post(
+    session: AsyncSession,
+    user_id: int,
+    name: str,
+    product: str,
+    products_count: int,
+    condition: str,
+    price: int,
+    currency: str,
+    city: str,
+    is_delivery_company: str,
+    comment: str,
+    phone_number: str,
+    telegram_username: str,
+) -> Post:
+    post = Post(
+        user_id=user_id,
+        name=name,
+        product=product,
+        products_count=products_count,
+        condition=condition,
+        price=price,
+        currency=currency,
+        city=city,
+        is_delivery_company=is_delivery_company,
+        comment=comment,
+        phone_number=phone_number,
+        telegram_username=telegram_username,
+    )
+
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+
+    return Post
