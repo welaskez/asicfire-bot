1e87d21fa1a7af665a1b6d630619db915dff3df9
welaskez <nazar.nikitenko.03@mail.ru>
2024-07-30 07:51:29 +0500

    add create user cond

--- bot/handlers/user/start.py
+++ bot/handlers/user/start.py
@@ -3,6 +3,10 @@ from aiogram.filters import Command
 
 from keyboards.inline import inline_keyboard, inline_button
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.crud.user_requests import get_user_by_tg_id, create_user
+
 from utils.messages import start
 
 from config import BASE_DIR
@@ -11,7 +15,15 @@ router = Router()
 
 
 @router.message(Command("start"))
-async def newsletter_cmd(message: types.Message):
+async def newsletter_cmd(message: types.Message, session: AsyncSession):
+    user = await get_user_by_tg_id(session=session, tg_id=message.from_user.id)
+    if user:
+        pass
+    if not user:
+        await create_user(
+            session=session,
+            tg_id=message.from_user.id,
+        )
     await message.answer_photo(
         photo=types.FSInputFile(f"{BASE_DIR}/imgs/img.jpg"),
         caption=start,
