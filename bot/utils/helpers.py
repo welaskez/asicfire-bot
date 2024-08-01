8bbdbc2e1da65f614492e52ba4b67068a52bf289
welaskez <nazar.nikitenko.03@mail.ru>
2024-08-01 12:43:12 +0500

    fix funcs

--- bot/utils/helpers.py
+++ bot/utils/helpers.py
@@ -1,8 +1,23 @@
 from datetime import datetime, timezone
 
-from db.models import User
-
 from .messages import post_template
+
+
+def is_post_limit_expired(expiration_time: datetime | None) -> bool:
+    return (
+        expiration_time < datetime.now(timezone.utc)
+        if expiration_time is not None
+        else True
+    )
+
+
+def calculate_time_left(expiration_time: datetime) -> str:
+    time_left = expiration_time - datetime.now(timezone.utc)
+    hours = time_left.seconds // 3600
+    minutes = (time_left.seconds % 3600) // 60
+    seconds = time_left.seconds % 60
+
+    return f"{hours} часов {minutes} минут {seconds} секунд"
 
 
 def format_post(post: dict) -> str:
@@ -12,7 +27,7 @@ def format_post(post: dict) -> str:
         post.get("products_count"),
         post.get("condition"),
         post.get("price"),
-        "$" if post.get("currency") == "РУБ" else "₽",
+        "₽" if post.get("currency") == "РУБ" else "$",
         post.get("city"),
         post.get("is_delivery_company"),
         post.get("comment"),
@@ -23,16 +38,3 @@ def format_post(post: dict) -> str:
 
 def decode_redis_hash(hash_data: dict) -> dict:
     return {k.decode("utf-8"): v.decode("utf-8") for k, v in hash_data.items()}
-
-
-def is_post_limit_expired(user: User) -> bool:
-    if user.post_limits_expiration_time is None:
-        return False
-
-    now = datetime.now(timezone.utc)
-    expiration_time = user.post_limits_expiration_time
-
-    if expiration_time.tzinfo is None:
-        expiration_time = expiration_time.replace(tzinfo=timezone.utc)
-
-    return now > expiration_time
